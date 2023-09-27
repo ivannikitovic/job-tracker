3c5a6417075c672b95380cc244613ef41e29ed97
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-09-26 21:40:58 -0400

    add server.js

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Configure CORS
+const corsOptions = {
+    origin: "http://yourfrontenddomain.com", // Replace with frontend domain
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Enable cookies and other credentials
+};
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => { res.json({ 0: "hello world" }) });
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+const MongoClient = mongodb.MongoClient;
+MongoClient.connect(
+    process.env.MONGODB_URI,
+    {
+        maxPoolSize: 50,
+    }
+)
+.catch(err => {
+    console.log(`failed to connect to MongoDB: \n${err.stack}`)
+    process.exit(1);
+})
+.then(async client => {
+    console.log(`connected to ${client.options.srvHost}`);
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
+    });
+});
