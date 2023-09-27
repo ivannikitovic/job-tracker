ef0612727593414247f32012bcba0b97e98d2aee
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2023-09-27 00:18:35 -0400

    add jwt

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,4 +1,5 @@
 import UserModel from "../model/user.model.js";
+import jwt from "jsonwebtoken";
 
 export default class UserController {
     static async signUp(req, res, next) {
@@ -8,7 +9,7 @@ export default class UserController {
             if (user) {
                 res.status(409).json({ error: "user already exists" });
             } else {
-                const postResponse = await UserModel.addUser(email, password);
+                const postResponse = await UserModel.addUser(email, password); // TODO: hash password
                 res.json(postResponse);
             }
         } catch (e) {
@@ -23,8 +24,19 @@ export default class UserController {
             const user = await UserModel.getUser(email);
             if (!user) {
                 res.status(404).json({ error: "user does not exist" });
-            } else if (user.password === password) {
-                res.json(user);
+            } else if (user.password === password) { // sign in successful
+
+                const payload = {
+                    userId: user._id,
+                };
+
+                const token = jwt.sign(
+                    payload, 
+                    process.env.JWT_SECRET, 
+                    { expiresIn: "2h" }
+                );
+
+                res.status(200).json({ token: token });
             } else {
                 res.status(401).json({ error: "incorrect password" });
             }
